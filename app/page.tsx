853e23634ac452aac6f1f33e2ecb75b4ab576fce
gustacoding <gustatsch@gmail.com>
2024-08-05 20:19:40 -0300

    chore: add git hooks setupgitt

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "./_components/ui/button";
-const name = 'test'
-const Home = () => {
-  return <button>test</button>;
-};
+import { Button } from "./_components/ui/button"
 
-export default Home;
+const Home = () => {
+  return <Button>test</Button>
+}
+
+export default Home
